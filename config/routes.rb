019b203cfeb6b390ac52a774431cded679665005
Alexei Danchenkov <alexeid@scientology.net>
2023-09-18 11:46:35 -0700

    add posts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :about, only: [:index]
   get :about, to: "about#index"
+  resources :posts
 
   # Defines the root path route ("/")
   root "about#index"
